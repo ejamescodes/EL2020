c9eaac8e75b7f088cc361ee64291450bc80a66fa
N03217280 <ernestperez97@gmail.com>
2020-03-04 13:18:39 -0500

    added blink_temp.py

--- code/blink_temp.py
+++ code/blink_temp.py
@@ -19,12 +19,13 @@ blinkTime = 7
 #Initialize the GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(redPin,GPIO.OUT)
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 def oneBlink(pin):
 	GPIO.output(pin,True)
 	time.sleep(blinkDur)
 	GPIO.output(pin,False)
 	time.sleep(blinkDur)
+
 def readF(tempPin):
 	humidity, temperature = Adafruit_DHT.read_retry(tempSensor,tempPin)
 	temperature = temperature * 9/5.0 +32
@@ -33,16 +34,17 @@ def readF(tempPin):
 	else:
 		print('Error Reading Sensor')
 	return tempFahr
+
 try:
 	while True:
 		input_state = GPIO.input(buttonPin)
-		if input_state == True:
+		if input_state == GPIO.HIGH:
 			for i in range (blinkTime):
 				oneBlink(redPin)
-				time.sleep(.2)
-				data = readF(tempPin)
-				print (data)
-			input_state = False
+			time.sleep(.2)
+			data = readF(tempPin)
+			print (data)
+
 except KeyboardInterrupt:
 	os.system('clear')
 	print('Thanks for Blinking and Thinking!')

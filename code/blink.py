e6eace6dda87db31a0ec1c96ffe96fe239bf53a8
BuildTools <unconfigured@null.spigotmc.org>
2020-05-02 00:19:34 -0400

    made shit work

--- code/blink.py
+++ code/blink.py
@@ -3,32 +3,77 @@ import RPi.GPIO as GPIO
 import time
 import signal
 import os
+import sqlite3 as mydb
 
 #Initialize the GPIO
+
+tch = 12
+snd = 16
+vib = 20
+ir = 21
+
+alarm = 26
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(21,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(alarm,GPIO.OUT)
+GPIO.setup(tch,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(snd,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(vib,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(ir,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+arm = 0
+trigger = 0
 
 #This function will make the light blink once
-def blinkOnce(pin):
-	GPIO.output(pin,True)
-	time.sleep(.1)
-	GPIO.output(pin,False)
-	time.sleep(.1)
-
-#Call the blinkOnce function above in a loop	
-try:
+def ring():
 	while True:
-		input_state = GPIO.input(21)
-		if GPIO.input(21) == GPIO.HIGH:
-			print('push')
-			for i in range (10):
-				blinkOnce(17)
-			time.sleep(.2)
+		for i in range (3):
+			GPIO.output(alarm,False)
+			time.sleep(.1)
+			GPIO.output(alarm,True)
+			time.sleep(.1)
 
+		time.sleep(.5)
+
+def disarm():
+	arm = 0
+	trigger = 0
+	GPIO.output(alarm, True)
+
+def sensorStatus():
+	if GPIO.input(snd) or GPIO.input(vib) or GPIO.input(ir):
+		trigger = 1
+
+def clearLog():
+	os.system('rm ../log/sensorLog.db')
+
+con = mydb.connect('../log/sensorLog.db')
+cur = con.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS sensorLog(Date INTTEGER, Arm INTEGER, Trigger INTEGER)""")
+
+
+
+#Call the blinkOnce function above in a loop
+try:
+	GPIO.output(alarm, True)
+	while True:
+		if GPIO.input(tch) == GPIO.HIGH:
+			arm = 1
+		if arm == 1:
+			sensorStatus()
+			if GPIO.input(snd) or GPIO.input(vib) or GPIO.input(ir):
+				trigger = 1
+			if trigger == 1:
+				cur.execute('INSERT INTO sensorLog (Date, Arm, Trigger) VALUES(?,?,?)', (time.strftime('%Y-%m-%d %H:%M:%S'), arm, trigger))
+				print(time.strftime('%Y‐%m‐%d %H:%M:%S')+" Arm: %d Trigger: %d\n" %(arm, trigger))
+				ring()
+		print(GPIO.input(ir))
+		print(time.strftime('%Y‐%m‐%d %H:%M:%S')+" Arm: %d Trigger: %d\n" %(arm, trigger))
+		time.sleep(.5)
 
 #Cleanup the GPIO when done
 except KeyboardInterrupt:
+	clearLog()
 	os.system('clear')
 	print('--END--')
 	GPIO.cleanup()
